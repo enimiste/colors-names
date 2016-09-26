c7ab308c37736beee7183cce25069d458fd4554d
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2016-09-26 14:38:33 +0100

    Use symfony crawler to parse w3school colors page result

--- src/Crawler.php
+++ src/Crawler.php
@@ -1,7 +1,11 @@
 <?php
 
 
-$url = 'http://www.w3schools.com/colors/colors_names.asp';
+use Symfony\Component\DomCrawler\Crawler;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$base_url = 'http://www.w3schools.com/colors/';
+$url = 'colors_names.asp';
 $output = __DIR__ . '/output/colors_names.php';
 
 
@@ -15,6 +19,30 @@ function fetch_colors($content)
     } else return [];
 }
 
+/**
+ * @param $url
+ * @return array
+ */
+function crawl_w3school($url)
+{
+    echo 'Crawling ' . $url . PHP_EOL;
+    $content = file_get_contents($url);
+    $crawler = new Crawler($content);
+    $content = $crawler->filter('.w3-table-all');
+    if ($content->count() > 0) {
+        $content = $content->first()->html();
+        $colors = fetch_colors($content);
+        $next_link = $crawler->filter('.nextprev > .w3-right');
+        if ($next_link->count() > 0) {
+            $next_link = $next_link->first()->attr('href');
+        } else $next_link = '';
+        echo count($colors) . ' found.' . PHP_EOL;
+        return [$colors, $next_link];
+    }
+    return [[], ''];
+}
+
+
 $php = <<<EOP
 	<?php
 
@@ -22,9 +50,11 @@ $php = <<<EOP
 
 EOP;
 
-$content = file_get_contents($url);
-$colors = fetch_colors($content);
-
+$colors = [];
+while (!empty($url)) {
+    list($res, $url) = crawl_w3school($base_url . $url);
+    $colors = array_merge($colors, $res);
+}
 $php .= array_reduce(array_keys($colors), function ($carry, $name) use ($colors) {
     return $carry . "['name'=>'$name',  'hex'=>'#{$colors[$name]}'],\n";
 });
@@ -32,3 +62,5 @@ $php .= array_reduce(array_keys($colors), function ($carry, $name) use ($colors)
 $php .= '];';
 
 file_put_contents($output, $php);
+
+echo 'End.' . PHP_EOL;
